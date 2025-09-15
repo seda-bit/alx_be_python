730b7f33178c611f59d2bfdddb7709867e4980b3
seda-bit <evanssedaa@gmail.com>
2025-09-15 16:07:13 +0300

    0. Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 5
+
+# Addition
+sum_result = number1 + number2
+print("Addition of 10 and 5 is ", sum_result)
+
+# Subtraction
+subtraction_result = number1 - number2  
+print("Subtraction of 10 and 5 is ", subtraction_result)
+
+# Multiplication
+multiplication_result = number1 * number2
+print("Multiplication of 10 and 5 is ", multiplication_result)
+
+
